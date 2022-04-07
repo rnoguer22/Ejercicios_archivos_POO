4ac801772961be9e6b541ae4eeea002187701002
rnoguer22 <rnogugon@myuax.com>
2022-04-07 19:34:44 +0200

    Aplicamos POO ya que estamos en el tema de POO

--- Clases/Apartado_1.py
+++ Clases/Apartado_1.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-def apartado1():
-    datos = pd.read_csv("calificaciones.csv", header = 0)
-    lista = []
-    lista_parcial_1 = list(datos["Parcial1"])
-    lista_parcial_2 = list(datos["Parcial2"])
-    lista_asistencia = list(datos["Asistencia"])
-    for i in range(1,17):
-        diccionario = {["Parcial 1: " + lista_parcial_1[i], "Parcial 2: " + lista_parcial_2[i]] : lista_asistencia[i]}
-        lista.append(diccionario)
-    return lista
+class Apartado1:
+    def __init__(self):
+        pass
 
-apartado1()
+    def apartado1(self):
+        datos = pd.read_csv("calificaciones.csv", header = 0)
+        lista = []
+        lista_parcial_1 = list(datos["Parcial1"])
+        lista_parcial_2 = list(datos["Parcial2"])
+        lista_asistencia = list(datos["Asistencia"])
+        for i in range(1,17):
+            diccionario = {["Parcial 1: " + lista_parcial_1[i], "Parcial 2: " + lista_parcial_2[i]] : lista_asistencia[i]}
+            lista.append(diccionario)
+        return lista
+
+resultado = Apartado1()
+print (resultado.apartado1())
