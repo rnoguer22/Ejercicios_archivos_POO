dd5042a7a08b7b9b5bb8d06d68aa468eda5e0e8b
rnoguer22 <rnogugon@myuax.com>
2022-04-07 19:56:33 +0200

    Completamos la clase apartado2 La funcion apartado2 contiene errores, ya que es heredada de apartado1 y este contiene fallos que no encuentro

--- Clases/Apartado_2.py
+++ Clases/Apartado_2.py
@@ -6,3 +6,11 @@ class Apartado2(Apartado1):
     def __init__(self, file):
         super().__init__(file)
     
+    def apartado2(self):
+        datos = pd.read_csv(self.file, header = 0)
+        lista = self.apartado1()
+        nota_final = 0.3*datos["Parcial1"] + 0.3*datos["Parcial2"] + 0.4*datos["OrdinarioPracticas"]
+        for i in range(1,17):
+            nuevo_par = list(nota_final)[i]
+            lista[i][0].append(nuevo_par)
+        return lista
