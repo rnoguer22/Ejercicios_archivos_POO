9281e4e5ec8be6a094419208974f5b0aa8f62890
rnoguer22 <rnogugon@myuax.com>
2022-04-07 19:32:30 +0200

    Funcion apartado1 Esta funcion da un key error ya que no reconoce que Parcial1 sea una columna del archivo .csv No entiendo por que, no encuentro el fallo :(

--- Clases/Apartado_1.py
+++ Clases/Apartado_1.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
-datos = pd.read_csv("calificaciones.csv", header = 0)
-print (datos)
+def apartado1():
+    datos = pd.read_csv("calificaciones.csv", header = 0)
+    lista = []
+    lista_parcial_1 = list(datos["Parcial1"])
+    lista_parcial_2 = list(datos["Parcial2"])
+    lista_asistencia = list(datos["Asistencia"])
+    for i in range(1,17):
+        diccionario = {["Parcial 1: " + lista_parcial_1[i], "Parcial 2: " + lista_parcial_2[i]] : lista_asistencia[i]}
+        lista.append(diccionario)
+    return lista
+
+apartado1()
